a040dd6385926c994bbb1be14a619a0fff37d562
Jacob <jacobmartinez3d@gmail.com>
2020-11-02 14:08:24 -0800

    - include python 3.9 in setup.py config.

--- setup.py
+++ setup.py
@@ -10,26 +10,21 @@ setuptools.setup(
     author_email="jacob@magnetic-lab.com",
     description="A test application for recording and performing math on temperature readings.",
     license="GNU General Public License v3 or later (GPLv3+)",
-     platforms=["POSIX", "Windows"],
-     py_modules=["temptracker"],
+    platforms=["POSIX", "Windows"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jacobmartinez3d/temptracker",
     packages=setuptools.find_packages(include="temptracker"),
     classifiers=[
-             "Development Status :: 2 - Pre-Alpha",
+            "Development Status :: 2 - Pre-Alpha",
             "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.1",
-            "Programming Language :: Python :: 3.2",
-            "Programming Language :: Python :: 3.3",
-            "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: POSIX"
     ],
-    python_requires=">=2.7, <3.9"
+    python_requires=">=2.7, <=3.9"
 )
